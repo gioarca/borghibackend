e4ea053402b7efe4793bdcf4789fcabf0452e285
gioarca <giorgio.arcamone93@gmail.com>
2024-04-08 16:11:36 +0200

    New port added

--- src/app.js
+++ src/app.js
@@ -34,7 +34,10 @@ mongoose
 
 // middlewares
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://main--borghisud.netlify.app/"
+  ); // only for production
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
